d2f87348e5d58d82d891a5a8d51426f8c747515a
Stein Rustad <steru@eg.no>
2021-05-06 13:32:39 +0200

    Merged PR 9386: Re-added a example for using .NET full framework for using auth flow against

---
+++ LindbakAccountServices/Sport/Readme.txt
@@ -0,0 +1,22 @@
+﻿
+
+Lindbak Account Services Integration Example
+
+The following example project illustrates how to integrate with LAS for gain the following features. The application needs
+to run using SSL on https://localhost:44326/ to work with the configured LAS tenant endpoints.
+
+
+* Login
+See startup.cs. Standard OpenId connect middleware is used to enable authentication against the test authority.
+
+* Logout
+See HomeController Logout action.
+
+* Profile Maintenance
+See HomeController EditProfile action on how to redirect to Profile page (authentication should be handled automatically)
+
+* Change password
+See HomeController ChangePassword action. Open endpoint that requires passing the userid (retrieved from sub claim).
+
+
+An online example can be found here: https://lrslindbaksporttest.azurewebsites.net
---
+++ LindbakAccountServices/Sport/Startup.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Helpers;
+using System.IdentityModel.Tokens;
+using System.Collections.Generic;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.IdentityModel.Protocols;
+using Microsoft.Owin.Security;
+using System.Security.Claims;
+
+[assembly: OwinStartup(typeof(Sport.Startup))]
+
+namespace Sport
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // todo: replace with serilog
+            //LogProvider.SetCurrentLogProvider(new DiagnosticsTraceLogProvider());
+
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = "sub";
+            JwtSecurityTokenHandler.InboundClaimTypeMap = new Dictionary<string, string>();
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "Cookies"
+            });
+
+
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            {
+                Authority = "https://sport.inv.no/sport/identity",
+
+                ClientId = "WebShop",
+                ResponseType = "id_token token",
+                Scope = "openid email profile member",
+                RedirectUri = "https://localhost:44326",
+                PostLogoutRedirectUri = "https://localhost:44326",
+                //RedirectUri = "https://lrslindbaksporttest.azurewebsites.net",
+                //PostLogoutRedirectUri = "https://lrslindbaksporttest.azurewebsites.net",
+                SignInAsAuthenticationType = "Cookies",
+                UseTokenLifetime = true,
+
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                {
+                    SecurityTokenValidated = n =>
+                   {
+                       // keep the id_token for logout
+                       n.AuthenticationTicket.Identity.AddClaim(new Claim("id_token", n.ProtocolMessage.IdToken));
+
+                       // keep access token for api requests
+                       n.AuthenticationTicket.Identity.AddClaim(new Claim("access_token", n.ProtocolMessage.AccessToken));
+
+                       return Task.FromResult(0);
+                   },
+
+                    RedirectToIdentityProvider = n =>
+                    {
+                        // Pass id_token for safe logout
+                        if (n.ProtocolMessage.RequestType == OpenIdConnectRequestType.LogoutRequest)
+                        {
+                            var idTokenHint = n.OwinContext.Authentication.User.FindFirst("id_token");
+
+                            if (idTokenHint != null)
+                            {
+                                n.ProtocolMessage.IdTokenHint = idTokenHint.Value;
+                            }
+                        }
+
+                        return Task.FromResult(0);
+                    }
+                }
+            });
+        }
+    }
+}
